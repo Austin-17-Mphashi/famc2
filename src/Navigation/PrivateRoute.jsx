e8d354b55bee6d90ef66b57dbbc065fc7a4e922b
Austin-17-Mphashi <mphashiaustin@gmail.com>
2023-06-01 09:36:38 +0200

    changed title

--- src/Navigation/PrivateRoute.jsx
+++ src/Navigation/PrivateRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet } from "react-router-dom"
 import { useAuth } from "../Context/AuthContext"
 
 export default function PrivateRoute({ component: Component, ...rest }) {
+  
   const { currentUser } = useAuth()
 
   return (
